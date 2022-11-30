888398bbe6a1876bc9337778c977349f0f7fdbc6
wrafael91 <wrafaelmp91@gmail.com>
2022-11-30 10:24:15 -0500

    agregando configuración token y bcrypt para encriptar passwords

--- backend/controllers/veterinarioController.js
+++ backend/controllers/veterinarioController.js
@@ -1,18 +1,14 @@
 import VeterinarioModel from "../models/Veterinario.js"
 
-
-// const registrar = async(req, res) => {
-//     try {
-//         const veterinario = new VeterinarioModel(req.body)
-//         const veterinarioGuardado = await veterinario.save()
-//         res.json({msg:"Probando desde api/veterinarios"})
-//     } catch (error) {
-//         console.log(error)
-//     }
-// }
-
-
 const registrar = async(req, res) => {
+    const {email} = req.body
+    //Prevenir usuarios duplicados
+    const existeUsuario = await VeterinarioModel.findOne({email})
+    
+    if(existeUsuario){
+        const error = new Error("Usuario ya registrado")
+        return res.status(400).json({msg: error.message})
+    }
 
     try {     
         const veterinario = new VeterinarioModel(req.body)
@@ -28,5 +24,23 @@ const perfil = async(req, res) => {
     res.json({msg: "consultando perfil..."})
 } 
 
+const confirmar = async (req, res) => {
+    const {token} = req.params
+    const usuarioConfirmar = await VeterinarioModel.findOne({token})
+    if(!usuarioConfirmar){
+        const error = new Error('token no valido')
+        return res.status(404).json({msg: error.message})
+    }
 
-export {registrar, perfil}
+    try {
+        usuarioConfirmar.token = null
+        usuarioConfirmar.confirmado = true
+        await usuarioConfirmar.save()
+        res.json({msg: 'Usuario confirmado correctamente'})
+    } catch (error) {
+        console.log(error)
+    }
+    console.log(usuarioConfirmar)
+}
+
+export {registrar, perfil, confirmar}
--- backend/models/Veterinario.js
+++ backend/models/Veterinario.js
@@ -1,4 +1,6 @@
-import mongoose from "mongoose";
+import mongoose from "mongoose"
+import bcrypt from 'bcrypt'
+import generarId from "../helpers/generarId.js"
 
 const veterinarioSchema = mongoose.Schema({
     nombre: {
@@ -31,12 +33,21 @@ const veterinarioSchema = mongoose.Schema({
         required: true
     },
     token: {
-        type: String
+        type: String,
+        default: generarId()
     },
     confirmado: {
         type: Boolean,
         default: false
     }
+})
+
+veterinarioSchema.pre('save', async function(next) {
+    if(!this.isModified("password")){
+        next()
+    }
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
 })
 
 const VeterinarioModel = mongoose.model("Veterinario", veterinarioSchema)
